7ec167b90dd7f98608f03d84e3a09d894b99a62e
acolombo09 <acolombo0911@gmail.com>
2023-07-17 16:21:08 +0200

    rework_with_for_in

--- js/script.js
+++ js/script.js
@@ -23,67 +23,75 @@ Buon lavoro!
 
 // creo l'array di oggetti con le informazioni fornite
 const teamMembers = [
-{
+  {
     firstName: "Wayne",
     lastName: "Barnett",
     role: "Founder & CEO",
     profilePic: "wayne-barnett-founder-ceo.jpg",
-},
-{
+  },
+  {
     firstName: "Angela",
     lastName: "Caroll",
     role: "Chief Editor",
     profilePic: "angela-caroll-chief-editor.jpg",
-},
-{
+  },
+  {
     firstName: "Walter",
     lastName: "Gordon",
     role: "Office Manage",
     profilePic: "walter-gordon-office-manager.jpg",
-},
-{
+  },
+  {
     firstName: "Angela",
     lastName: "Lopez",
     role: "Social Media Manager",
     profilePic: "angela-lopez-social-media-manager.jpg",
-},
-{
+  },
+  {
     firstName: "Scott",
     lastName: "Estrada",
     role: "Developer",
     profilePic: "scott-estrada-developer.jpg",
-},
-{
+  },
+  {
     firstName: "Barbara",
     lastName: "Ramos",
     role: "Graphic Designer",
     profilePic: "scott-estrada-developer.jpg",
-},
+  },
 ];
 
-// provo col let in
-// for (let key in teamMembers) {
-
-//   console.log(key); // ottengo l'indice della chiave dell'object
-//   console.log(teamMembers[key]); // ottengo il valore della chiave dell'object
-// }
 let teamInfoContainer = document.querySelector('#team-info');
-let teamInfoHTML = "";
 
 // Stampo su console (per ogni membro del team) info di nome, ruolo e stringa foto
-// PER OGNI membro del team, stampo le info uso for each
-teamMembers.forEach(function(member) {
-    console.log("Name: " + member.firstName + ' ' + member.lastName);
-    console.log("Role: " + member.role);
-    console.log("Profile Pic: " + member.profilePic);
-    // Infine, aggiungo una riga vuota per la leggibilità dell'output di ogni membro
-    console.log("----------------------------------");
-    // Aggiungo le informazioni del membro come stringhe alla stringa HTML
-    teamInfoHTML += "<p>Name: " + member.firstName + " " + member.lastName + "</p>" +
-    "<p>Role: " + member.role + "</p>" +
-    "<p>Photo: " + member.profilePic + "</p>";
-});
+// PER OGNI membro del team, stampo le info 
+// Creo schede per ogni membro del team usando il ciclo for-in
+for (let i in teamMembers) {
+  let member = teamMembers[i];
 
-// Aggiungo la stringa HTML contenente le informazioni di tutti i membri al 
-// container del team sul DOM
-teamInfoContainer.innerHTML = teamInfoHTML;
+  console.log("Name: " + member.firstName + " " + member.lastName);
+  console.log("Role: " + member.role);
+  console.log("Profile Pic: " + member.profilePic);
+  console.log("----------------------------------");
+
+  // Creo una scheda per il membro del team
+  let cardHTML =
+    '<div class="card">' +
+    '<img class="card-img-top" src="img/' +
+    member.profilePic +
+    '">' +
+    '<div class="card-body">' +
+    "<h5 class='card-title'>" +
+    member.firstName +
+    " " +
+    member.lastName +
+    "</h5>" +
+    "<p class='card-text'>" +
+    member.role +
+    "</p>" +
+    "</div>" +
+    "</div>";
+
+  // Aggiungo la scheda al contenitore del team sul DOM
+  teamInfoContainer.innerHTML += cardHTML;
+}
